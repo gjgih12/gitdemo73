8bcdcae45d34d36c592988344e0e7d59a55107cd
gjgih12 <474526043@qq.com>
2019-07-03 17:04:37 +0800

    提交了一个car实体类

---
+++ src/main/java/com/jk/model/Car.java
@@ -0,0 +1,24 @@
+package com.jk.model;
+
+public class Car {
+
+    private Integer carId;
+    private String carName;
+
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+}
