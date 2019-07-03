6d64a2c6245fff280302b5a2107c72f3105069c6
gjgih12 <474526043@qq.com>
2019-07-03 16:31:18 +0800

    第一次提交git示例代码

---
+++ src/main/java/com/jk/GitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jk/GitdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
